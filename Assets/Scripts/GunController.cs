292d397fbdd40fd4749db10710fb21b104835e7f
Neoptolemospapadiofantous <Neoptolemos.papadiofantous@gmail.com>
2020-09-03 19:29:54 +0300

    Gun & Player Rotation
    Imported a gun and implemented its rotation with the player mouse cursor.

---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotateGun();
+    }
+
+    private void RotateGun()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        Vector3 gunPosition = Camera.main.WorldToScreenPoint(transform.position);
+        mousePosition.x = mousePosition.x - gunPosition.x;
+        mousePosition.y = mousePosition.y - gunPosition.y;
+        float gunAngle = Mathf.Atan2(mousePosition.y, mousePosition.x) * Mathf.Rad2Deg;
+
+        if(Camera.main.ScreenToWorldPoint(Input.mousePosition).x <transform.position.x)
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(180f, 0f, -gunAngle));
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, gunAngle));
+        }
+    }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour{
+
+    //Config
+    [Header("Input Settings:")]
+    [SerializeField] float runSpeed = 5f;
+
+    //State
+    bool isAlive = true;
+
+    //Cached Component references
+
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+        Rotate();
+    }
+
+    private void Run()
+    {
+        //Horizontal movement of the player.
+        float controlThrow = Input.GetAxis("Horizontal");
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        GetComponent<Rigidbody2D>().velocity = playerVelocity;
+
+        //Running Animation
+        bool playerIsMoving = Math.Abs(GetComponent<Rigidbody2D>().velocity.x) > Mathf.Epsilon;
+        GetComponent<Animator>().SetBool("Running", playerIsMoving);
+    }
+    private void Shoot()
+    {
+
+    }
+    private void Rotate()
+    {
+        Vector3 gunPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (gunPosition.x < transform.position.x)
+        {
+            transform.eulerAngles = new Vector3(transform.rotation.x, 180f, transform.rotation.z);
+        }
+        else
+        {
+            transform.eulerAngles = new Vector3(transform.rotation.x, 0f, transform.rotation.z);
+        }
+    }
+}
+
